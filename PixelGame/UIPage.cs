96c5a5ba4571429b2789aeac0773545c7f418695
DionDune <tomas.reynolds935@gmail.com>
2023-10-27 23:39:33 +0100

    Created the UIPage Class

---
+++ PixelGame/UIPage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PixelGame
+{
+    internal class UIPage
+    {
+        public string Type { get; set; }
+
+        public List<UIItem> UIItems { get; set; }
+
+        public UIPage()
+        {
+            Type = "Default";
+
+            UIItems = new List<UIItem>();
+        }
+    }
+}
