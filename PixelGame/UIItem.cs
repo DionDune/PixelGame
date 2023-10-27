f6fc6a0d99a3fd5583562a14535bca4782843799
DionDune <tomas.reynolds935@gmail.com>
2023-10-27 23:21:31 +0100

    Created the UIItem Class

---
+++ PixelGame/UIItem.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PixelGame
+{
+    internal class UIItem
+    {
+        public string Type { get; set; }
+
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public int CentreX { get; set; }
+        public int CentreY { get; set; }
+
+        public int BorderWidth { get; set; }
+
+        public Color BaseColor { get; set; }
+        public Color BorderColor { get; set; }
+        public Color HighlightedColor { get; set; }
+
+        public int PercentFull { get; set; }
+        public int FillDirection { get; set; }
+
+        public UIItem()
+        {
+            Type = "Button";
+
+            X = 0;
+            Y = 0;
+
+            Width = 10;
+            Height = 10;
+
+            CentreX = 5;
+            CentreY = 5;
+
+            BorderWidth = 0;
+
+            BaseColor = Color.Purple;
+            BorderColor = Color.Black;
+            HighlightedColor = Color.Gold;
+
+            PercentFull = 100;
+            FillDirection = 0;
+        }
+    }
+}
